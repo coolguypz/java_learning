0bd39a7bab7729fb3de1f613a0251f3a7192d6ab
peter zhang <peterzhang@peters-iMac.attlocal.net>
2022-02-01 23:09:37 -0800

    day 1

---
+++ journal.java
@@ -0,0 +1,12 @@
+import javax.sound.sampled.SourceDataLine;
+
+/**
+ * journal
+ */
+public class journal {
+// My Journal 
+  public static void main(String[] args) {
+    System.out.println("\n");
+    System.out.println("My Journal 1.0\n");
+  }
+}
